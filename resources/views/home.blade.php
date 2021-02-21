894799aab691114528f09e2ae8f7065a8b8e3ecc
dule <dulee797@gmail.com>
2021-02-21 21:14:31 +0100

    initial commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,10 @@
          <img draggable="false" src="https://instagram.fbeg8-1.fna.fbcdn.net/v/t51.2885-19/s150x150/97566921_2973768799380412_5562195854791540736_n.jpg?_nc_ht=instagram.fbeg8-1.fna.fbcdn.net&amp;_nc_ohc=0oVoPC0dcYwAX9LkAT-&amp;tp=1&amp;oh=243ce2eeccfcf863a7615e7b0b8c6caf&amp;oe=6057B4E7" class="rounded-circle">
       </div>
       <div class="col-9 pt-5">
-         <div><h1>{{$user->username}}</h1></div>
+         <div class="d-flex justify-content-between align-items-baseline">
+            <h1>{{$user->username}}</h1>
+            <a href="#">Add New Post</a>
+         </div>
          <div class="d-flex">
            <div class="pr-5"><strong>number</strong> followers</div>
            <div class="pr-5"><strong>number</strong> following</div>
