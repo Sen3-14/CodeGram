7e98e4bab0a5b224c8449423074bf995e197e7ee
dule <dule>
2021-03-04 22:21:36 +0100

    edit profile

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -7,10 +7,27 @@ use App\Models\User;
 
 class ProfilesController extends Controller
 {
+  //nacin 1
     public function index($user){
       $user = User::findOrFail($user);
       return view('profiles.index',[
         'user' => $user
       ]);
+    }
+
+//nacin 2
+    public function edit(User $user){
+         return view('profiles.edit',compact('user'));
+    }
+
+    public function update(User $user){
+      $data = request()->validate([
+        'title' => 'required',
+        'description' => 'required',
+        'url' => 'url',
+        'image' => '',
+      ]);
+        auth()->user()->profile->update($data);
+        return redirect("/profile/{$user->id}");
     }
 }
